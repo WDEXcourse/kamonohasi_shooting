4aca18e0ed51f640669b2a78b9e232aa41412e3b
WDEXcourse <wdkawasaki.online@gmail.com>
2021-09-08 19:05:31 +0900

    門を完成さた

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform targetObj;
+    private float colliderOffset;
+    private Animator animator;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform mytransform = this.transform;
+        var distance = Vector3.Distance(transform.position, targetObj.position);
+
+        if (distance < 40)
+        {
+            Debug.Log("i");
+            animator.SetBool("monn1", true);
+        }
+        if (distance > 100)
+        {
+            animator.SetBool("monn3", true);
+        }
+
+
+
+
+    }
+}
+
